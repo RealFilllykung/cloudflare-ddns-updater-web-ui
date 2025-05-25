a06761f7006f1d726615c57b001a010df8318e05
RealFilllykung <filllykunggm@gmail.com>
2025-05-25 21:57:29 +0700

    feat: backend project

---
+++ backend/src/test/java/realfilllykung/cloudflare/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package realfilllykung.cloudflare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
