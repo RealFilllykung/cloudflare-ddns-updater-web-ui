889a43ff049b2e8a288590133c66c680089aff8f
RealFilllykung <filllykunggm@gmail.com>
2025-05-28 23:50:01 +0700

    feat: record controller setup

--- backend/src/main/java/realfilllykung/cloudflare/backend/controller/RecordController.java
+++ backend/src/main/java/realfilllykung/cloudflare/backend/controller/RecordController.java
@@ -1,33 +1,36 @@
 package realfilllykung.cloudflare.backend.controller;
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
+
+
 @RestController
-public class CredentialController {
-    @GetMapping("/credentials")
-    public String getCredentials() {
-        return "Hello World";
+public class RecordController {
+    @GetMapping("records")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
     }
 
-    @PostMapping("/credentials")
+    @PostMapping("records")
     public String postMethodName(@RequestBody String entity) {
         return entity;
     }
 
-    @PutMapping("path/{id}")
+    @PutMapping("records/{id}")
     public String putMethodName(@PathVariable String id, @RequestBody String entity) {
         return entity;
     }
 
-    @DeleteMapping("/credentials/{id}")
+    @DeleteMapping("records/{id}")
     public String deleteMethodName(@PathVariable String id) {
-        return "Deleted credential with id: " + id;
-    }
-    
+        return "Deleted record with id: " + id;
+    }   
 }
