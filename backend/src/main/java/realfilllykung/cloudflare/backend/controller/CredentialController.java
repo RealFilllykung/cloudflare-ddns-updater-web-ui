053edc4bc648469de5ac64dde6a81526fe2fa808
RealFilllykung <filllykunggm@gmail.com>
2025-05-31 02:21:02 +0700

    feat: add validator annotation jakarta

--- backend/src/main/java/realfilllykung/cloudflare/backend/controller/CredentialController.java
+++ backend/src/main/java/realfilllykung/cloudflare/backend/controller/CredentialController.java
@@ -3,21 +3,31 @@ package realfilllykung.cloudflare.backend.controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import realfilllykung.cloudflare.backend.models.Credential;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.validation.Valid;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
 @RestController
 public class CredentialController {
+
+
     @GetMapping("/credentials")
     public String getCredentials() {
         return "Hello World";
     }
 
     @PostMapping("/credentials")
-    public String postMethodName(@RequestBody String entity) {
-        return entity;
+    public Credential postMethodName(@Valid @RequestBody Credential credential) {
+        return credential;
     }
 
     @PutMapping("path/{id}")
---
+++ backend/src/main/java/realfilllykung/cloudflare/backend/models/Credential.java
@@ -0,0 +1,21 @@
+package realfilllykung.cloudflare.backend.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+public class Credential { 
+    @NotBlank(message = "credentialName should not be blank") @NotNull(message = "credentialName should not be null") @Size(max = 50, message = "credentialName should not exceed 50 characters")
+    public String credentialName;
+    
+    @NotBlank(message = "zoneIdentifierId should not be blank") @NotNull(message = "zoneIdentifierId should not be null")
+    public String zoneIdentifierId;
+    
+    @NotBlank(message = "apiKey should not be blank") @NotNull(message = "apiKey should not be null")
+    public String apiKey;
+
+    @NotBlank(message = "email should not be blank")  @NotNull(message = "email should not be null") @Email(message = "email should be a valid email address")
+    public String email; 
+}
