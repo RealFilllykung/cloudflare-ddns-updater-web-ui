46a7638e9b8dc6246c7be63daa58c58fe8c90b01
RealFilllykung <filllykunggm@gmail.com>
2025-05-31 11:56:35 +0700

    feat: class security

--- backend/src/main/java/realfilllykung/cloudflare/backend/model/Credential.java
+++ backend/src/main/java/realfilllykung/cloudflare/backend/model/Credential.java
@@ -9,20 +9,20 @@ public class Credential {
     @NotBlank(message = "credentialName should not be blank")
     @NotNull(message = "credentialName should not be null")
     @Size(max = 50, message = "credentialName should not exceed 50 characters")
-    public String credentialName;
+    private String credentialName;
     
     @NotBlank(message = "zoneIdentifierId should not be blank")
     @NotNull(message = "zoneIdentifierId should not be null")
-    public String zoneIdentifierId;
+    private String zoneIdentifierId;
     
     @NotBlank(message = "apiKey should not be blank")
     @NotNull(message = "apiKey should not be null")
-    public String apiKey;
+    private String apiKey;
 
     @NotBlank(message = "email should not be blank")
     @NotNull(message = "email should not be null")
     @Email(message = "email should be a valid email address")
-    public String email; 
+    private String email; 
 
     public Credential(String credentialName, String zoneIdentifierId, String apiKey, String email) {
         this.credentialName = credentialName;
